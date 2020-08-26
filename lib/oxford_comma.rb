f78303ed030a4975f1af80abbf8c95740b569810
Corey Martin <coreyjmartin2@gmail.com>
2020-08-26 02:55:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@
 #.join turns an array into a string
 #put a comma in between all elements of an array EXCEPT
 #last element of the array should have an #and in front 
-#array.last calls on last element of an array
+#array[-1] calls on last element of an array
 
 def oxford_comma(array)
  if array.size == 1
@@ -12,6 +12,6 @@ def oxford_comma(array)
   array.join (" and ")
  else array.size >= 3
   array[-1] = "and " + array[-1]
-  array.join(', ')
+  array.join(", ")
  end
 end
